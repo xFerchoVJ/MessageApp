3d263b3348f202eebf76f3d436fab5456c550c78
xFerchoVJ <fer_elsword@hotmail.com>
2021-10-07 18:05:08 -0500

    Setup ActionCable and amount it to the routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
